c816e17fd63a2c8e418b07a0726f4175c4800390
drishti-cs1251 <dginfo1234@gmail.com>
2025-05-29 13:19:43 +0530

    Include menu in home section

--- little-lemon/src/pages/Home.jsx
+++ little-lemon/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import About from "../components/About";
 import Testimonials from "../components/Testimonials";
+import Menu from '../components/Menu';
+
 
 const Home = () => {
   return (
     <>
       <About />
+      <Menu />
       <Testimonials />
     </>
   );
